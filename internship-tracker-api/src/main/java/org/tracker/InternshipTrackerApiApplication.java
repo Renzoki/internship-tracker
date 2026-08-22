f30f0c3760aeb8e480d264ca6bd10ac95e3685c6
Renz <renzonifico@gmail.com>
2026-08-22 23:49:13 +0800

    init

---
+++ internship-tracker-api/src/main/java/org/tracker/InternshipTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package org.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipTrackerApiApplication.class, args);
+    }
+
+}
---
+++ internship-tracker-api/src/test/java/org/tracker/InternshipTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
