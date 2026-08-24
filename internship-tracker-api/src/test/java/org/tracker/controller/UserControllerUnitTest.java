f5f8683ac90fca6ee3faf12ddb7c73efe7c8e223
Renz <renzonifico@gmail.com>
2026-08-24 13:27:44 +0800

    test: add integration tests for GET and POST/ users endpoints

--- internship-tracker-api/src/test/java/org/tracker/controller/UserControllerUnitTest.java
+++ internship-tracker-api/src/test/java/org/tracker/controller/UserControllerUnitTest.java
@@ -12,18 +12,19 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.tracker.exception.UserNotFoundException;
 import org.tracker.mapper.UserMapper;
+import org.tracker.model.business.CreateUserCommand;
 import org.tracker.model.entities.User;
 import org.tracker.service.UserService;
-
 import java.util.List;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
 @Import(UserMapper.class)
-public class UserControllerIntegrationTest {
+public class UserControllerUnitTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -113,18 +114,99 @@ public class UserControllerIntegrationTest {
 
     @Test
     public void getUserById_userDoesNotExist() throws Exception {
-        UUID nonexistendId = UUID.fromString("22222222-2222-2222-2222-222222222222");
+        UUID nonexistentId = UUID.fromString("22222222-2222-2222-2222-222222222222");
 
-        when(userService.getUserById(nonexistendId))
-                .thenThrow(new UserNotFoundException(nonexistendId));
+        when(userService.getUserById(nonexistentId))
+                .thenThrow(new UserNotFoundException(nonexistentId));
 
         RequestBuilder request = MockMvcRequestBuilders
-                .get("/users/" + nonexistendId)
+                .get("/users/" + nonexistentId)
                 .accept(MediaType.APPLICATION_JSON);
-
 
         mockMvc.perform(request)
                 .andExpect(status().isNotFound());
     }
 
+    @Test
+    public void postUser_validRequestBody() throws Exception {
+        UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
+        when(userService.createNewUser(any(CreateUserCommand.class)))
+                .thenReturn(new User(mockId,
+                        "Renz",
+                        "Tabuzo",
+                        "renzonifico@gmail.com",
+                        null,
+                        null));
+
+        String requestBody = """
+        {
+            "firstName": "Renz",
+            "lastName": "Tabuzo",
+            "email": "renzonifico@gmail.com",
+            "password": "password123"
+        }
+        """;
+
+        String expectedJson = """
+        {
+            "id": "%s",
+            "firstName": "Renz",
+            "lastName": "Tabuzo",
+            "email": "renzonifico@gmail.com"
+        }
+        """.formatted(mockId);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(content().json(expectedJson));
+    }
+
+    @Test
+    public void postUser_emptyFields() throws Exception {
+        String requestBody = """
+        {
+            "firstName": "",
+            "lastName": "",
+            "email": "",
+            "password": ""
+        }
+        """;
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void postUser_invalidEmailField() throws Exception {
+        String requestBody = """
+        {
+            "firstName": "Renz",
+            "lastName": "Tabuzo",
+            "email": "renzonificoAtgmail.com",
+            "password": "password123"
+        }
+        """;
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+                .accept(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isBadRequest());
+    }
 }
