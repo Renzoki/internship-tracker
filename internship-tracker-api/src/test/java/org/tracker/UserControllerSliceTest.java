76fbf7a72aac5e56e7b7a0198cbf6fdda46ec920
Renz <renzonifico@gmail.com>
2026-09-01 01:25:31 +0800

    test: add JWT authentication to user controller slice tests

--- internship-tracker-api/src/test/java/org/tracker/UserControllerSliceTest.java
+++ internship-tracker-api/src/test/java/org/tracker/UserControllerSliceTest.java
@@ -1,5 +1,6 @@
 package org.tracker;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
@@ -10,15 +11,15 @@ import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.tracker.configuration.JwtAuthenticationFilter;
+import org.tracker.configuration.SecurityConfig;
 import org.tracker.controller.UserController;
-import org.tracker.exception.UserNotFoundException;
 import org.tracker.mapper.UserMapper;
 import org.tracker.model.business.CreateUserCommand;
 import org.tracker.model.business.UpdateUserCommand;
 import org.tracker.model.entities.User;
+import org.tracker.service.JwtService;
 import org.tracker.service.UserService;
-
-import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -26,8 +27,17 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
-@Import(UserMapper.class)
+@Import({
+        SecurityConfig.class,
+        JwtAuthenticationFilter.class,
+        UserMapper.class
+})
 public class UserControllerSliceTest {
+
+    private UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
+    private String mockToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+            "eyJzdWIiOiIxMTExMTExMS0xMTExLTExMTEtMTExMS0xMTExMTExMTExMTEiLCJuYW1lIjoiUmVueiBUYWJ1em8iLCJpYXQiOjE1MTYyMzkwMjJ9." +
+            "SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
     @Autowired
     private MockMvc mockMvc;
@@ -35,58 +45,22 @@ public class UserControllerSliceTest {
     @MockitoBean
     UserService userService;
 
+    @MockitoBean
+    JwtService jwtService;
+
     @MockitoSpyBean
     private UserMapper userMapper;
 
-    @Test
-    public void getAllUsers_oneUser() throws Exception {
+    @BeforeEach
+    public void jwtSetup(){
         UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
-        when(userService.getAllUsers()).thenReturn(
-                List.of(new User(mockId,
-                        "Renz",
-                        "Tabuzo",
-                        "renzonifico@gmail.com",
-                        null,
-                        null))
-        );
-
-        String expectedJson = """
-        [{
-            "id": "%s",
-            "firstName": "Renz",
-            "lastName": "Tabuzo",
-            "email": "renzonifico@gmail.com"
-        }]
-        """.formatted(mockId);
-
-        RequestBuilder request = MockMvcRequestBuilders
-                .get("/users")
-                        .accept(MediaType.APPLICATION_JSON);
-
-        mockMvc.perform(request)
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(expectedJson));
-    }
-
-    @Test
-    public void getAllUsers_noUsers() throws Exception {
-        when(userService.getAllUsers()).thenReturn(List.of());
-
-        RequestBuilder request = MockMvcRequestBuilders
-                .get("/users")
-                .accept(MediaType.APPLICATION_JSON);
-
-        mockMvc.perform(request)
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json("[]"));
+        when(jwtService.extractId(any(String.class))).thenReturn(mockId);
+        when(jwtService.extractEmail(any(String.class))).thenReturn("renzonifico@gmail.com");
     }
 
     @Test
     public void getUserById_userExists() throws Exception {
-        UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
-        when(userService.getUserById(mockId)).thenReturn(
+        when(userService.getUserById(any(UUID.class))).thenReturn(
                 new User(mockId,
                         "Renz",
                         "Tabuzo",
@@ -105,9 +79,8 @@ public class UserControllerSliceTest {
         """.formatted(mockId);
 
         RequestBuilder request = MockMvcRequestBuilders
-                .get("/users/" + mockId)
-                .accept(MediaType.APPLICATION_JSON);
-
+                .get("/users/self")
+                .header("Authorization", "Bearer " + mockToken);
 
         mockMvc.perform(request)
                 .andExpect(status().isOk())
@@ -116,23 +89,7 @@ public class UserControllerSliceTest {
     }
 
     @Test
-    public void getUserById_userDoesNotExist() throws Exception {
-        UUID nonexistentId = UUID.fromString("22222222-2222-2222-2222-222222222222");
-
-        when(userService.getUserById(nonexistentId))
-                .thenThrow(new UserNotFoundException(nonexistentId));
-
-        RequestBuilder request = MockMvcRequestBuilders
-                .get("/users/" + nonexistentId)
-                .accept(MediaType.APPLICATION_JSON);
-
-        mockMvc.perform(request)
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
     public void postUser_validRequestBody() throws Exception {
-        UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
         when(userService.createNewUser(any(CreateUserCommand.class)))
                 .thenReturn(new User(mockId,
                         "Renz",
@@ -215,7 +172,6 @@ public class UserControllerSliceTest {
 
     @Test
     public void updateUser_fullUpdate() throws Exception {
-        UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
         when(userService.updateExistingUser(any(UpdateUserCommand.class)))
                 .thenReturn(new User(
                         mockId,
@@ -245,7 +201,8 @@ public class UserControllerSliceTest {
                 """.formatted(mockId);
 
         RequestBuilder request = MockMvcRequestBuilders
-                .put("/users/" + mockId)
+                .put("/users/self")
+                .header("Authorization", "Bearer " + mockToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestBody)
                 .accept(MediaType.APPLICATION_JSON);
@@ -258,7 +215,6 @@ public class UserControllerSliceTest {
 
     @Test
     public void updateUser_partialUpdate() throws Exception {
-        UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
         when(userService.updateExistingUser(any(UpdateUserCommand.class)))
                 .thenReturn(new User(
                         mockId,
@@ -287,7 +243,8 @@ public class UserControllerSliceTest {
                 """.formatted(mockId);
 
         RequestBuilder request = MockMvcRequestBuilders
-                .put("/users/" + mockId)
+                .put("/users/self")
+                .header("Authorization", "Bearer " + mockToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestBody)
                 .accept(MediaType.APPLICATION_JSON);
@@ -300,13 +257,13 @@ public class UserControllerSliceTest {
 
     @Test
     public void updateUser_blankUpdate() throws Exception {
-        UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
         String requestBody = """
             { }
             """;
 
         RequestBuilder request = MockMvcRequestBuilders
-                .put("/users/" + mockId)
+                .put("/users/self")
+                .header("Authorization", "Bearer " + mockToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestBody);
 
@@ -317,7 +274,6 @@ public class UserControllerSliceTest {
 
     @Test
     public void updateUser_containsNotNullButBlankFields() throws Exception {
-        UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
         String requestBody = """
             { 
                 "firstName": "Renz",
@@ -328,7 +284,8 @@ public class UserControllerSliceTest {
             """;
 
         RequestBuilder request = MockMvcRequestBuilders
-                .put("/users/" + mockId)
+                .put("/users/self")
+                .header("Authorization", "Bearer " + mockToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestBody);
 
@@ -339,8 +296,6 @@ public class UserControllerSliceTest {
 
     @Test
     public void updateUser_invalidEmailField() throws Exception {
-        UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
-
         String requestBody = """
         {
             "firstName": "Renz",
@@ -351,7 +306,8 @@ public class UserControllerSliceTest {
         """;
 
         RequestBuilder request = MockMvcRequestBuilders
-                .put("/users/" + mockId)
+                .put("/users/self")
+                .header("Authorization", "Bearer " + mockToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestBody);
 
@@ -361,29 +317,12 @@ public class UserControllerSliceTest {
 
     @Test
     public void deleteUser_userExists() throws Exception {
-        UUID mockId = UUID.fromString("11111111-1111-1111-1111-111111111111");
-
         RequestBuilder request = MockMvcRequestBuilders
-                .delete("/users/" + mockId);
+                .delete("/users/self")
+                .header("Authorization", "Bearer " + mockToken);
 
         mockMvc.perform(request)
                 .andExpect(status().isNoContent());
-
-        verify(userService, times(1)).deleteUserById(any(UUID.class));
-    }
-
-    @Test
-    public void deleteUser_userDoesNotExist() throws Exception {
-        UUID nonExistentId = UUID.fromString("11111111-1111-1111-1111-111111111111");
-
-        doThrow(new UserNotFoundException(nonExistentId))
-                .when(userService).deleteUserById(nonExistentId);
-
-        RequestBuilder request = MockMvcRequestBuilders
-                .delete("/users/" + nonExistentId);
-
-        mockMvc.perform(request)
-                .andExpect(status().isNotFound());
 
         verify(userService, times(1)).deleteUserById(any(UUID.class));
     }
